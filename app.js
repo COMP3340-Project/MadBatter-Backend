14401313a63c044603e362eaaa623be8f869427f
Nisarg Patel <nisarg38@gmail.com>
2022-06-18 01:36:52 -0400

    Heroku-Connection

--- app.js
+++ app.js
@@ -10,11 +10,20 @@ var app = express();
 // middleware
 app.use(express.json());
 
+app.listen(process.env.PORT || 5000, function () {
+  console.log("Server is running on localhost 5000");
+});
+
 // Setting up the routes
 const products = require("./routes/product");
 
+app.get("/", (req, res) => {
+  res.send("I am Alive!! --MadBatter BackEnd");
+});
+
 app.use("/api/v1/products", products);
 
+//
 //
 //
 
